8365dab06764534cb68cd4e3f2a712a268427486
cicalese <cindom@gmail.com>
2023-03-09 15:33:12 -0500

    Clean up sql

--- db/apiary.sql
+++ db/apiary.sql
@@ -1,41 +1,4 @@
--- MariaDB dump 10.19  Distrib 10.5.18-MariaDB, for debian-linux-gnu (x86_64)
---
--- Host: localhost    Database: apiary
--- ------------------------------------------------------
--- Server version	10.5.18-MariaDB-0+deb11u1-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `_yoyo_migration`
---
-
-DROP TABLE IF EXISTS `_yoyo_migration`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `_yoyo_migration` (
-  `id` varchar(255) NOT NULL,
-  `ctime` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `apiary_bot_log`
---
-
 DROP TABLE IF EXISTS `apiary_bot_log`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `apiary_bot_log` (
   `log_id` int(11) NOT NULL AUTO_INCREMENT,
   `log_date` datetime NOT NULL,
@@ -46,15 +9,8 @@ CREATE TABLE `apiary_bot_log` (
   PRIMARY KEY (`log_id`),
   KEY `idx_log_date` (`log_date`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=18627857 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `apiary_multiprops`
---
 
 DROP TABLE IF EXISTS `apiary_multiprops`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `apiary_multiprops` (
   `website_id` int(11) NOT NULL,
   `t_name` varchar(255) NOT NULL,
@@ -64,15 +20,8 @@ CREATE TABLE `apiary_multiprops` (
   `occurrences` int(11) NOT NULL,
   PRIMARY KEY (`website_id`,`t_name`,`t_value`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `apiary_website_logs`
---
 
 DROP TABLE IF EXISTS `apiary_website_logs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `apiary_website_logs` (
   `log_id` int(11) NOT NULL AUTO_INCREMENT,
   `website_id` int(11) NOT NULL,
@@ -88,30 +37,10 @@ CREATE TABLE `apiary_website_logs` (
   KEY `idx_log_date` (`log_date`) USING BTREE,
   KEY `idx_website_id_log_date` (`website_id`,`log_date`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=27056086 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Temporary table structure for view `apiary_website_logs_summary`
---
 
 DROP TABLE IF EXISTS `apiary_website_logs_summary`;
-/*!50001 DROP VIEW IF EXISTS `apiary_website_logs_summary`*/;
-SET @saved_cs_client     = @@character_set_client;
-SET character_set_client = utf8;
-/*!50001 CREATE VIEW `apiary_website_logs_summary` AS SELECT
- 1 AS `website_id`,
-  1 AS `log_count`,
-  1 AS `log_date_last`,
-  1 AS `lot_date_first` */;
-SET character_set_client = @saved_cs_client;
-
---
--- Table structure for table `smwextinfo`
---
 
 DROP TABLE IF EXISTS `smwextinfo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `smwextinfo` (
   `website_id` int(11) NOT NULL,
   `capture_date` datetime NOT NULL,
@@ -147,15 +76,8 @@ CREATE TABLE `smwextinfo` (
   `format_ul` int(11) NOT NULL,
   PRIMARY KEY (`website_id`,`capture_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `smwinfo`
---
 
 DROP TABLE IF EXISTS `smwinfo`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `smwinfo` (
   `website_id` int(11) NOT NULL,
   `capture_date` datetime NOT NULL,
@@ -171,15 +93,8 @@ CREATE TABLE `smwinfo` (
   `errorcount` int(11) DEFAULT NULL,
   PRIMARY KEY (`website_id`,`capture_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `smwinfo_formats`
---
 
 DROP TABLE IF EXISTS `smwinfo_formats`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `smwinfo_formats` (
   `website_id` int(11) NOT NULL,
   `capture_date` datetime NOT NULL,
@@ -187,15 +102,8 @@ CREATE TABLE `smwinfo_formats` (
   `format_count` int(11) NOT NULL,
   PRIMARY KEY (`website_id`,`capture_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `statistics`
---
 
 DROP TABLE IF EXISTS `statistics`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `statistics` (
   `website_id` int(11) NOT NULL,
   `capture_date` datetime NOT NULL,
@@ -214,15 +122,8 @@ CREATE TABLE `statistics` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci
  PARTITION BY HASH (`website_id`)
 PARTITIONS 15;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `statistics_daily`
---
 
 DROP TABLE IF EXISTS `statistics_daily`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `statistics_daily` (
   `website_id` int(11) NOT NULL,
   `website_date` date NOT NULL,
@@ -247,15 +148,8 @@ CREATE TABLE `statistics_daily` (
   `smw_declaredpropcount_last` int(11) NOT NULL,
   PRIMARY KEY (`website_id`,`website_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `statistics_weekly`
---
 
 DROP TABLE IF EXISTS `statistics_weekly`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `statistics_weekly` (
   `website_id` int(11) NOT NULL,
   `website_date` date NOT NULL,
@@ -280,15 +174,8 @@ CREATE TABLE `statistics_weekly` (
   `smw_declaredpropcount_last` int(11) NOT NULL,
   PRIMARY KEY (`website_id`,`website_date`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `website_status`
---
 
 DROP TABLE IF EXISTS `website_status`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `website_status` (
   `website_id` int(11) NOT NULL,
   `check_every_limit` int(11) DEFAULT 60,
@@ -301,33 +188,3 @@ CREATE TABLE `website_status` (
   `last_skin_hash` char(64) DEFAULT NULL,
   PRIMARY KEY (`website_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Final view structure for view `apiary_website_logs_summary`
---
-
-/*!50001 DROP VIEW IF EXISTS `apiary_website_logs_summary`*/;
-/*!50001 SET @saved_cs_client          = @@character_set_client */;
-/*!50001 SET @saved_cs_results         = @@character_set_results */;
-/*!50001 SET @saved_col_connection     = @@collation_connection */;
-/*!50001 SET character_set_client      = utf8 */;
-/*!50001 SET character_set_results     = utf8 */;
-/*!50001 SET collation_connection      = utf8_general_ci */;
-/*!50001 CREATE ALGORITHM=UNDEFINED */
-/*!50013 DEFINER=`root`@`192.168.189.13` SQL SECURITY DEFINER */
-/*!50001 VIEW `apiary_website_logs_summary` AS select `apiary`.`apiary_website_logs`.`website_id` AS `website_id`,count(0) AS `log_count`,max(`apiary`.`apiary_website_logs`.`log_date`) AS `log_date_last`,min(`apiary`.`apiary_website_logs`.`log_date`) AS `lot_date_first` from `apiary_website_logs` group by `apiary`.`apiary_website_logs`.`website_id` */;
-/*!50001 SET character_set_client      = @saved_cs_client */;
-/*!50001 SET character_set_results     = @saved_cs_results */;
-/*!50001 SET collation_connection      = @saved_col_connection */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2023-03-09 15:34:46
